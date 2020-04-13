35a36aaccc9078acb6cec01d3a08f8e1c3d9303b
Max Godefroy <max@godefroy.net>
2020-04-13 10:28:31 +0200

    Workspace setup

---
+++ src/calculator.js
@@ -0,0 +1,17 @@
+/**
+ * @file calculator.js
+ * @author Max Godefroy <max@godefroy.net>
+ */
+
+export class Calculator
+{
+    constructor(jobModifiers, levelModifiers)
+    {
+        self.data = {
+            "jobModifiers": jobModifiers,
+            "levelModifiers": levelModifiers
+        }
+    }
+
+
+}
---
+++ src/functions.js
@@ -0,0 +1,10 @@
+/**
+ * @file functions.js
+ * @author Max Godefroy <max@godefroy.net>
+ */
+
+
+export function potency(value)
+{
+    return value / 100
+}
